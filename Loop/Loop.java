9adea2e5108f418fbd414a334e03912abab8954d
TanviDhanajiShinde <tanvishinde316@gmail.com>
2026-08-30 00:32:20 +0530

    Add files via upload

---
+++ Loop/Loop.java
@@ -0,0 +1,131 @@
+import java.util.*;
+class Loop{
+    public static void main(String args[]){
+        // int counter = 1;
+        // int n = 0;
+        // while (counter <= 10){
+        //     System.out.println("hello world "+ counter);
+        //     counter++;
+        // }
+
+        // print number 1 to n 
+         Scanner sc = new Scanner(System.in);
+        // int n ;
+        // int a = 0;
+        // n = sc.nextInt();
+        // while(a <= n){
+        //     System.out.println(a);
+        //     a++;
+        // }
+
+    // int n;
+    // int c=1,sum=0;
+    // n = sc.nextInt();
+    // while(c <= n){
+    //     sum=sum+c;
+    //     c++;
+    // }
+    // System.out.println(sum);
+// for(int i = 0;i<=10;i++){
+//     System.out.println(i);
+// }
+
+// Printing the line pattern 
+// for(int i = 1; i <= 4; i++){
+//     System.out.println("* * * *");
+// }
+// int n = 1;
+// while(n <= 4){
+//     System.out.println("* * * *");
+//     n++;
+// }
+// int n = 10899;
+// int lastdigit = 0;
+// while(n > 0){
+//     lastdigit=n % 10 ;
+//     System.out.print(lastdigit);
+//     n = n/10;
+  
+// }
+// System.out.println();
+
+// int n = 10899;
+// int rev = 0;
+// while(n > 0){
+//     int lastdigit = n % 10;
+//     rev = (rev*10)+lastdigit;
+//     n=n/10;
+// }
+// System.out.println(rev);
+
+// DO WHILE LOOP 
+// int n=1;
+// int c=0;
+// do{
+//     System.out.println(n);
+//     n++;
+// }while(n<=10);
+
+// BREAK STATEMENT 
+// for(int i=0;i<=5;i++){
+//     if(i==3){
+//         break;
+//     }
+//     System.out.println("hi "+i);
+// }
+// System.out.println("i am out of control");
+
+// do{
+//     System.out.println("Enter your no : ");
+//     int n = sc.nextInt();
+//     if(n % 10 == 0){
+//         break;
+//     }
+//     System.out.println(n);
+// }while(true);
+
+// CONTINUE STATEMENT 
+// for(int i = 1;i<=5;i++){
+//     if(i==3){
+//         continue;
+//     }
+//         System.out.println(i);
+// }
+//     }
+// do{
+//     System.out.println("Enter the Number");
+//     int n= sc.nextInt();
+//     if(n%10==0){
+//         continue;
+//     }
+//     System.out.println(n);
+// }while(true);
+
+// check no is prime or not
+
+// int n= sc.nextInt();
+
+// if(n==2)
+// {
+//     System.out.println("No is Prime");
+// }else{
+//     boolean isPrime = true;
+//     for(int i = 2;i<=Math.sqrt(n);i++){
+//         if(n%i==0){
+//             isPrime=false;
+//         }
+//     }
+
+//     if(isPrime==true){
+//     System.out.println("No is prime");
+// }
+// else{
+//     System.out.println("no is not prime");
+// }
+
+// }
+
+
+
+} 
+}
